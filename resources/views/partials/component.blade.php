878d43e5c9eee49ac7eb32ac2a4ee714473aa974
RangerX <rangerx.1985@gmail.com>
2016-05-17 16:37:51 +0400

    * SLA as progress bar

--- resources/views/partials/component.blade.php
+++ resources/views/partials/component.blade.php
@@ -5,12 +5,24 @@
     {{ $component->name }}
     @endif
 
-    @if($component->show_sla)
-        SLA: {{ $component->sla }} / {{ $component->accepted_sla }}
-    @endif
-
     @if($component->description)
     <i class="ion ion-ios-help-outline help-icon" data-toggle="tooltip" data-title="{{ $component->description }}" data-container="body"></i>
+    @endif
+
+    @if($component->show_sla && $component->acceptable_sla > 0)
+    {{-- calculating values for progress bar display --}}
+    <?php
+            $green_sect_perc = $component->sla / $component->acceptable_sla * 100;
+            if ($green_sect_perc < 0) $green_sect_perc = 0; if ($green_sect_perc > 100) $green_sect_perc = 100;
+    ?>
+    <div class="progress">
+        <div class="progress-bar progress-bar-success progress-bar-striped" style="width: {{ $green_sect_perc }}%">
+        </div>
+        <div class="progress-bar progress-bar-danger progress-bar-striped" style="width: {{ (100 - $green_sect_perc) }}%">
+            {{ number_format(($component->acceptable_sla -  $component->sla), 4, '.', '') }}
+        </div>
+    </div>
+    SLA: {{ $component->sla }} / {{ $component->acceptable_sla }}
     @endif
 
     <div class="pull-right">
